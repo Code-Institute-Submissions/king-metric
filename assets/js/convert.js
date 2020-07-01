a9a0eb6d7c1b822e582c17d834d499665f594e6c
Developer Hope <me@devhope.co>
2020-07-01 11:07:15 +0000

    Improved property switcher and started unit switcher

--- assets/js/convert.js
+++ assets/js/convert.js
@@ -1,25 +1,37 @@
-function UnitConverter() {
-  const enteredValue = parseInt(document.getElementById("inputValue").value);
-  let property = document.getElementById("properties").value;
-  let unit = document.getElementById("units").value;
+function PropertyConverter() {
+  const property = document.getElementById("properties").value;
 
   switch (property) {
     case "length":
       document.getElementById("units").innerHTML =
-        '<option value="inch" selected>Inch</option><option value="foot">Foot</option><option value="yard">Yard</option><option value="mile">Mile</option>';
+        '<option value="inch">Inch</option><option value="foot">Foot</option><option value="yard">Yard</option><option value="mile">Mile</option>';
       break;
     case "area":
       document.getElementById("units").innerHTML =
-        '<option value="sqinch" selected>Square Inch</option><option value="sqfoot">Square Foot</option><option value="sqyard">Square Yard</option><option value="sqmile">Square Mile</option>';
+        '<option value="sqinch">Square Inch</option><option value="sqfoot">Square Foot</option><option value="sqyard">Square Yard</option><option value="sqmile">Square Mile</option>';
       break;
     case "volume":
       document.getElementById("units").innerHTML =
-        '<option value="cuinch" selected>Cubic Inch</option><option value="cufoot">Cubic Foot</option><option value="flounce">Fluid Ounce</option><option value="gallon">Gallon</option>';
+        '<option value="cuinch">Cubic Inch</option><option value="cufoot">Cubic Foot</option><option value="flounce">Fluid Ounce</option><option value="gallon">Gallon</option>';
       break;
     case "weigth":
       document.getElementById("units").innerHTML =
-        '<option value="ounce" selected>Ounce</option><option value="pound">Pound</option><option value="stone">Stone</option><option value="ton">Ton</option>';
+        '<option value="ounce">Ounce</option><option value="pound">Pound</option><option value="stone">Stone</option><option value="ton">Ton</option>';
       break;
   }
+
+  return;
+}
+
+function UnitConverter() {
+  const enteredValue = parseInt(document.getElementById("inputValue").value);
+  const unit = document.getElementById("units").value;
+
+  switch (unit) {
+    case "inch":
+      document.getElementById("metricUnit").innerText = "Centimetre(s):";
+      break;
+  }
+
   return;
 }
