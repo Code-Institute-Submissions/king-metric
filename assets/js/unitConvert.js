1295eab60957a48f013463d125e588ff374fab42
Developer Hope <me@devhope.co>
2020-07-03 07:32:19 +0000

    Refactored function UnitConverter() in file unitConvert.js

--- assets/js/unitConvert.js
+++ assets/js/unitConvert.js
@@ -1,57 +1,26 @@
 function UnitConverter() {
-  const unit = document.getElementById("units").value;
+  const convertedName = {
+    inch: "Centimetre(s):",
+    foot: "Metre(s):",
+    yard: "Metre(s):",
+    mile: "Kilometre(s):",
+    sqinch: "Square Centimetre(s):",
+    sqfoot: "Square Metre(s):",
+    sqyard: "Square Metre(s):",
+    sqmile: "Square Kilometre(s):",
+    cuinch: "Cubic Centimetre(s):",
+    cufoot: "Cubic Metre(s):",
+    flounce: "Millilitre(s):",
+    gallon: "Litre(s):",
+    ounce: "Gram(s):",
+    pound: "Kilogram(s):",
+    stone: "Kilogram(s):",
+    ton: "Tonne(s):",
+  };
 
-  switch (unit) {
-    // Unit switch for Length
-    case "inch":
-      document.getElementById("metricUnit").innerText = "Centimetre(s):";
-      break;
-    case "foot":
-    case "yard":
-      document.getElementById("metricUnit").innerText = "Metre(s):";
-      break;
-    case "mile":
-      document.getElementById("metricUnit").innerText = "Kilometre(s):";
-      break;
+  const selectedUnit = document.getElementById("units").value;
 
-    //Unit switch for Area
-    case "sqinch":
-      document.getElementById("metricUnit").innerText = "Square Centimetre(s):";
-      break;
-    case "sqfoot":
-    case "sqyard":
-      document.getElementById("metricUnit").innerText = "Square Metre(s):";
-      break;
-    case "sqmile":
-      document.getElementById("metricUnit").innerText = "Square Kilometre(s):";
-      break;
-
-    //Unit switch for Volume
-    case "cuinch":
-      document.getElementById("metricUnit").innerText = "Cubic Centimetre(s):";
-      break;
-    case "cufoot":
-      document.getElementById("metricUnit").innerText = "Cubic Metre(s):";
-      break;
-    case "flounce":
-      document.getElementById("metricUnit").innerText = "Millilitre(s):";
-      break;
-    case "gallon":
-      document.getElementById("metricUnit").innerText = "Litre(s):";
-      break;
-
-    //Unit switch for Weigth
-    case "ounce":
-      document.getElementById("metricUnit").innerText = "Gram(s):";
-      break;
-    case "pound":
-    case "stone":
-      document.getElementById("metricUnit").innerText = "Kilogram(s):";
-      break;
-    case "ton":
-      document.getElementById("metricUnit").innerText = "Tonne(s):";
-      break;
-  }
+  document.getElementById("metricUnit").innerText = convertedName[selectedUnit];
 
   ValueConverter();
 
---
+++ assets/js/unitConvertBeforeRefactoring.js
@@ -0,0 +1,62 @@
+// This was the original code used for the function UnitConverter(). It worked fine. However, it was not optimal.
+// After refactoting, the outcome is what you can see in the file unitConvert.js.
+
+function UnitConverter() {
+  const unit = document.getElementById("units").value;
+
+  switch (unit) {
+    // Unit switch for Length
+    case "inch":
+      document.getElementById("metricUnit").innerText = "Centimetre(s):";
+      break;
+    case "foot":
+    case "yard":
+      document.getElementById("metricUnit").innerText = "Metre(s):";
+      break;
+    case "mile":
+      document.getElementById("metricUnit").innerText = "Kilometre(s):";
+      break;
+
+    //Unit switch for Area
+    case "sqinch":
+      document.getElementById("metricUnit").innerText = "Square Centimetre(s):";
+      break;
+    case "sqfoot":
+    case "sqyard":
+      document.getElementById("metricUnit").innerText = "Square Metre(s):";
+      break;
+    case "sqmile":
+      document.getElementById("metricUnit").innerText = "Square Kilometre(s):";
+      break;
+
+    //Unit switch for Volume
+    case "cuinch":
+      document.getElementById("metricUnit").innerText = "Cubic Centimetre(s):";
+      break;
+    case "cufoot":
+      document.getElementById("metricUnit").innerText = "Cubic Metre(s):";
+      break;
+    case "flounce":
+      document.getElementById("metricUnit").innerText = "Millilitre(s):";
+      break;
+    case "gallon":
+      document.getElementById("metricUnit").innerText = "Litre(s):";
+      break;
+
+    //Unit switch for Weigth
+    case "ounce":
+      document.getElementById("metricUnit").innerText = "Gram(s):";
+      break;
+    case "pound":
+    case "stone":
+      document.getElementById("metricUnit").innerText = "Kilogram(s):";
+      break;
+    case "ton":
+      document.getElementById("metricUnit").innerText = "Tonne(s):";
+      break;
+  }
+
+  ValueConverter();
+
+  return;
+}
