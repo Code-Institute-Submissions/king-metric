072c63665d7ebe980b7b608bf37699968de40065
Developer Hope <me@devhope.co>
2020-07-03 07:41:40 +0000

    Refactored function PropertyConverter() in file propertyConvert.js

--- assets/js/propertyConvert.js
+++ assets/js/propertyConvert.js
@@ -1,25 +1,19 @@
 function PropertyConverter() {
-  const property = document.getElementById("properties").value;
+  const convertedOptions = {
+    length:
+      '<option value="inch">Inch</option><option value="foot">Foot</option><option value="yard">Yard</option><option value="mile">Mile</option>',
+    area:
+      '<option value="sqinch">Square Inch</option><option value="sqfoot">Square Foot</option><option value="sqyard">Square Yard</option><option value="sqmile">Square Mile</option>',
+    volume:
+      '<option value="cuinch">Cubic Inch</option><option value="cufoot">Cubic Foot</option><option value="flounce">Fluid Ounce</option><option value="gallon">Gallon</option>',
+    weigth:
+      '<option value="ounce">Ounce</option><option value="pound">Pound</option><option value="stone">Stone</option><option value="ton">Ton</option>',
+  };
 
-  //Switch to read property and determine the unit options
-  switch (property) {
-    case "length":
-      document.getElementById("units").innerHTML =
-        '<option value="inch">Inch</option><option value="foot">Foot</option><option value="yard">Yard</option><option value="mile">Mile</option>';
-      break;
-    case "area":
-      document.getElementById("units").innerHTML =
-        '<option value="sqinch">Square Inch</option><option value="sqfoot">Square Foot</option><option value="sqyard">Square Yard</option><option value="sqmile">Square Mile</option>';
-      break;
-    case "volume":
-      document.getElementById("units").innerHTML =
-        '<option value="cuinch">Cubic Inch</option><option value="cufoot">Cubic Foot</option><option value="flounce">Fluid Ounce</option><option value="gallon">Gallon</option>';
-      break;
-    case "weigth":
-      document.getElementById("units").innerHTML =
-        '<option value="ounce">Ounce</option><option value="pound">Pound</option><option value="stone">Stone</option><option value="ton">Ton</option>';
-      break;
-  }
+  const selectedProperty = document.getElementById("properties").value;
+
+  document.getElementById("units").innerHTML =
+    convertedOptions[selectedProperty];
 
   UnitConverter();
 
---
+++ assets/js/propertyConvertBeforeRefactoring.js
@@ -0,0 +1,32 @@
+// This was the original code used for the function PropertyConverter(). It worked fine. However, it was not optimal.
+// After refactoting, the outcome is what you can see in the file propertyConvert.js.
+
+function PropertyConverter() {
+  const property = document.getElementById("properties").value;
+
+  //Switch to read property and determine the unit options
+  switch (property) {
+    case "length":
+      document.getElementById("units").innerHTML =
+        '<option value="inch">Inch</option><option value="foot">Foot</option><option value="yard">Yard</option><option value="mile">Mile</option>';
+      break;
+    case "area":
+      document.getElementById("units").innerHTML =
+        '<option value="sqinch">Square Inch</option><option value="sqfoot">Square Foot</option><option value="sqyard">Square Yard</option><option value="sqmile">Square Mile</option>';
+      break;
+    case "volume":
+      document.getElementById("units").innerHTML =
+        '<option value="cuinch">Cubic Inch</option><option value="cufoot">Cubic Foot</option><option value="flounce">Fluid Ounce</option><option value="gallon">Gallon</option>';
+      break;
+    case "weigth":
+      document.getElementById("units").innerHTML =
+        '<option value="ounce">Ounce</option><option value="pound">Pound</option><option value="stone">Stone</option><option value="ton">Ton</option>';
+      break;
+  }
+
+  UnitConverter();
+
+  ValueConverter();
+
+  return;
+}
